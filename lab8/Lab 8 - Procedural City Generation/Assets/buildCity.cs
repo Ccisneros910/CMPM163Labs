58498f5396fa4623bf93ba40b631999730d95bd3
Carlos Cisneros <33742776+Ccisneros910@users.noreply.github.com>
2020-05-26 11:24:53 -0700

    Lab 8
    - Screenshots moved

--- lab8/Lab 8 - Procedural City Generation/Assets/buildCity.cs
+++ lab8/Lab 8 - Procedural City Generation/Assets/buildCity.cs
@@ -17,24 +17,26 @@ public class buildCity : MonoBehaviour
         {
             for (int w = 0; w < mapWidth; w++)
             {
-                int result = (int)(Mathf.PerlinNoise(w/15.0f, h/15.0f) * 15);
+                int result = (int)(Mathf.PerlinNoise(w/15.0f, h/15.0f) * 10);
                 Vector3 pos = new Vector3(w * buildingFootprint, 0, h *buildingFootprint);
                 if (result < 1)
                     Instantiate(buildings[0], pos, Quaternion.identity);
                 else if (result < 2)
                     Instantiate(buildings[1], pos, Quaternion.identity);
-                else if (result < 3)
-                    Instantiate(buildings[2], pos, Quaternion.identity);
-                else if (result < 4)
-                    Instantiate(buildings[3], pos, Quaternion.identity);
                 else if (result < 5)
+                    Instantiate(buildings[2], pos, Quaternion.identity);
+                else if (result < 7)
+                    Instantiate(buildings[3], pos, Quaternion.identity);
+                else if (result < 10)
                     Instantiate(buildings[4], pos, Quaternion.identity);
+                /*
                 else if (result < 6)
                     Instantiate(buildings[5], pos, Quaternion.identity);
                 else if (result < 7)
                     Instantiate(buildings[6], pos, Quaternion.identity);
                 else if (result < 15)
                     Instantiate(buildings[7], pos, Quaternion.identity);
+                */
             }
         }
     }
