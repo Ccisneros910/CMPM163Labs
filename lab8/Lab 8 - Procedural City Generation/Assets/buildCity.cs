cf0f8ca59bce90852ae3b31aa2a07db1586623c6
Carlos Cisneros <33742776+Ccisneros910@users.noreply.github.com>
2020-05-26 22:40:54 -0700

    Lab 8 City Look
    - Taller buildings added and programmed to spawn farther away from each other

--- lab8/Lab 8 - Procedural City Generation/Assets/buildCity.cs
+++ lab8/Lab 8 - Procedural City Generation/Assets/buildCity.cs
@@ -8,6 +8,7 @@ public class buildCity : MonoBehaviour
     public int mapWidth = 30;
     public int mapHeight = 30;
     int buildingFootprint = 3;
+    int bldg1, bldg2, bldg3 = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class buildCity : MonoBehaviour
         {
             for (int w = 0; w < mapWidth; w++)
             {
-                int result = (int)(Mathf.PerlinNoise(w/15.0f, h/15.0f) * 10);
+                int result = (int)(Mathf.PerlinNoise(w/15.0f, h/15.0f) * 13);
                 Vector3 pos = new Vector3(w * buildingFootprint, 0, h *buildingFootprint);
                 if (result < 1)
                     Instantiate(buildings[0], pos, Quaternion.identity);
@@ -25,18 +26,34 @@ public class buildCity : MonoBehaviour
                     Instantiate(buildings[1], pos, Quaternion.identity);
                 else if (result < 5)
                     Instantiate(buildings[2], pos, Quaternion.identity);
-                else if (result < 7)
-                    Instantiate(buildings[3], pos, Quaternion.identity);
-                else if (result < 10)
-                    Instantiate(buildings[4], pos, Quaternion.identity);
-                /*
                 else if (result < 6)
-                    Instantiate(buildings[5], pos, Quaternion.identity);
+                    Instantiate(buildings[3], pos, Quaternion.identity);
                 else if (result < 7)
-                    Instantiate(buildings[6], pos, Quaternion.identity);
-                else if (result < 15)
+                { 
+                    if(bldg1%30 == 0)
+                        Instantiate(buildings[4], pos, Quaternion.identity);
+                    else
+                        Instantiate(buildings[7], pos, Quaternion.identity);
+                    bldg1++;
+                }
+                else if (result < 8)
+                {
+                    if(bldg2%70 == 0)
+                        Instantiate(buildings[5], pos, Quaternion.identity);
+                    else
+                        Instantiate(buildings[7], pos, Quaternion.identity);
+                    bldg2++;
+                }
+                else if (result < 9)
+                {
+                    if(bldg3%150 == 0)
+                        Instantiate(buildings[6], pos, Quaternion.identity);
+                    else
+                        Instantiate(buildings[7], pos, Quaternion.identity);
+                    bldg3++;
+                }
+                else if (result < 13)
                     Instantiate(buildings[7], pos, Quaternion.identity);
-                */
             }
         }
     }
